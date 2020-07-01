7d7c82bfc0d1df13990bb7490ca2c17e59b9c265
Muntaha Shams <muntahashams288@gmail.com>
2020-07-01 15:07:14 +0500

    Add files via upload

---
+++ class2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+muntaaha
+    </div>
+  );
+}
+
+export default App;
